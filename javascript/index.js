a72ae57c740d930d1e767d96840569265310ddfe
Mohammed Roni <mohammed_roni@apple.com>
2019-10-14 10:12:27 -0700

    deleted all files

--- javascript/index.js
+++
@@ -1,9 +0,0 @@
-const openSidePanel = () => {
-    var sideElemeent = 'sidePanel'
-    document.getElementById(sideElemeent).style.width = '150px';
-}
-
-const closeSidePanel = () => {
-    var sideElemeent = 'sidePanel'
-    document.getElementById(sideElemeent).style.width = '0px';
-}
